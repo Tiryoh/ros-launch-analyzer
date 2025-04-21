ac13d5ea6ffd3d170eb9e33bf84fd84b4fce036a
Daisuke Sato <tiryoh@gmail.com>
2025-04-22 02:30:20 +0900

    chore: Update repository URL in changelog config and version script

--- .github/scripts/update_version.sh
+++ .github/scripts/update_version.sh
@@ -3,4 +3,4 @@
 VERSION=$1
 
 sed -i -E 's/version = "[0-9\.]*"/version = "'$VERSION'"/g' pyproject.toml
-sed -i -E 's/__version__ = "[0-9\.]*"/__version__ = "'$VERSION'"/g' mcap_to_mp4/_version.py
+sed -i -E 's/__version__ = "[0-9\.]*"/__version__ = "'$VERSION'"/g' ros_launch_analyzer/_version.py
